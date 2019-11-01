e9dd4391c0b557f43e0b1417716f71adbd43525a
carlijnmaria <57219902+carlijnmaria@users.noreply.github.com>
2019-11-01 10:41:06 +0100

    Add files via upload

---
+++ opdracht5/sendmail.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlijn
+ * Date: 01/11/2019
+ * Time: 09:27
+ */
+
+//mail variabelen.
+$to = 'patrick.laranjoG@gmail.com';
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$header = 'From: ' . $_POST['from'];
+
+//mail versturen
+mail($to, $subject, $message, $header) or die ('Error mailing. Could not send your email.');
+
+//bevestiging voor de gebruiker
+echo 'Uw email is verzonden.';
+?>
+
